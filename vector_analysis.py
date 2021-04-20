f9b6716954ae2224997a6f79d8dc47160a332d33
Zachary <zbroadm1@swarthmore.edu>
2021-04-20 15:20:50 -0400

    got vector_analysis working but there seems to be a problem writing to files in the main pipeline

--- vector_analysis.py
+++ vector_analysis.py
@@ -1,16 +1,69 @@
 import numpy as np
 from sklearn.decomposition import PCA
+from scipy import stats
 from io import StringIO
 
-infile = "output/vectors/vectors_simTest.txt"
+nodelist = "input/real_input/dblp_yoj_2000_nodelist.txt"
+infile = "output/vectors/vectors_experiment1-2.txt"
 outfile = "output/analysis/pca.txt"
+outfile1 = "output/analysis/pearson.txt"
 
 def main():
     cleanFileObj = clean_vectors(infile)
-    X = np.loadtxt(cleanFileObj, delimiter=',')
-    print("Matrix: \n", X, "\n")
-    run_pca(X)
+    pearson_analysis(nodelist, cleanFileObj)
     return True
+
+
+#takes as input a numpy matrix, then performs PCA analysis on it
+#info on analysis: https://scikit-learn.org/stable/modules/generated/sklearn.decomposition.PCA.html
+def pca_analysis(file):
+    X = np.loadtxt(file, delimiter=',')
+    print("Matrix: \n", X, "\n")
+    pca2 = PCA(n_components=1)
+    pca2.fit(X)
+    write_pca("PCA2", pca2, outfile)
+    return True
+
+#info on analysis: https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.spearmanr.html
+def pearson_analysis(nodefile, vecfile):
+    ranksLst = np.loadtxt(nodefile, delimiter='; ', skiprows=1, usecols=4)
+    ranksArr = np.array(ranksLst)
+    #run pca
+    vectors = np.loadtxt(vecfile, delimiter=',')
+    pca2 = PCA(n_components=1)
+    pca2.fit(vectors)
+    #get components from PCA
+    components = np.reshape(pca2.components_, (pca2.components_.size,))
+
+    #build a list of tuples (rank, componentVal)
+    rankscomps=[]
+    for i in range(ranksLst.size):
+        rankscomps.append([ranksLst[i], components[i]])
+    #sort rankscomps by component values
+    rankscomps.sort(key=lambda t: t[1])
+    #get a list of the ranks sorted by component values
+    sortedRanksLst = extract_ith_tuple(rankscomps, 0)
+    #make into array to run pearson
+    sortedRanksArr = np.array(sortedRanksLst)
+    #run the pearson analysis
+    result = stats.pearsonr(ranksArr, sortedRanksArr)
+    #print results to file
+    with open(outfile1, 'a') as f:
+        out = "PEARSON ANALYSIS ON THE FOLLOWING DATA: \n"
+        out += "Node data: " + nodelist + "\n"
+        out += "Vector file: " + infile + "\n"
+        out += "Pearson's correlation coefficient: " + str(result[0]) + "\n"
+        out += "Two-tailed p-value: " + str(result[1]) + "\n"
+        out += "\n"
+        f.write(out)
+
+    return result
+
+def extract_ith_tuple(list, i):
+    out = []
+    for tuple in list:
+        out.append(tuple[i])
+    return out
 
 #reads in the vector file and removes trailing commas from each line
 #returns a StringIO object, which behaves like a file
@@ -23,33 +76,11 @@ def clean_vectors(filename):
             line = f.readline()
     return StringIO(out)
 
-#takes as input a numpy matrix, then performs PCA analysis on it
-#info on analysis: https://scikit-learn.org/stable/modules/generated/sklearn.decomposition.PCA.html
-def run_pca(matrix):
-    X = matrix
-    pca1 = PCA(n_components=X.shape[0])
-    pca1.fit(X)
-    write_pca("pca1", pca1, outfile)
-
-    pca2 = PCA(n_components=1)
-    pca2.fit(X)
-    write_pca("pca1", pca2, outfile)
-
-    print("------------------PCA1------------------\n")
-    print("components_: \n", pca1.components_, "\n")
-    print("explained_variance_: \n", pca1.explained_variance_, "\n")
-    print("explained_variance_ratio_: \n", pca1.explained_variance_ratio_, "\n")
-    print("------------------PCA2------------------\n")
-    print("components_: \n", pca2.components_, "\n")
-    print("explained_variance_: \n", pca2.explained_variance_, "\n")
-    print("explained_variance_ratio_: \n", pca2.explained_variance_ratio_, "\n")
-
-    return True
-
 def write_pca(name, pca, filename):
     with open(filename, 'a') as f:
         out = "------------------------- " + name + " -------------------------\n"
-        out += "components_:\n" + str(pca.components_) + "\n"
+        out += "n_components_:\n" + str(pca.n_components_) + "\n"
+        out += "components_:\n" + str(pca.components_.shape) + "\n" + str(pca.components_) + "\n"
         out += "explained_variance_:\n" + str(pca.explained_variance_) + "\n"
         out += "explained_variance_ratio_:\n" + str(pca.explained_variance_ratio_) + "\n"
         out += "\n\n"
