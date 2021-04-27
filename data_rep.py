bda8a2fadbd55d8745397b13154c0401bb2ed456
Zachary <zbroadm1@swarthmore.edu>
2021-04-27 10:09:38 -0400

    fixed heatmap and pushing config for full analysis

--- data_rep.py
+++ data_rep.py
@@ -1,6 +1,7 @@
 #implementation from https://www.python-graph-gallery.com/heatmap/ and
-#https://www.kite.com/python/docs/seaborn.heatmap and
+#https://www.kite.com/python/docs/seaborn.heatmap
 #https://blog.quantinsti.com/creating-heatmap-using-python-seaborn/
+#https://towardsdatascience.com/heatmap-basics-with-pythons-seaborn-fb92ea280a6c
 import seaborn
 import pandas as pd
 import numpy as np
@@ -17,8 +18,12 @@ def pcaHeatmap(datafile, outfile):
     df = pd.read_csv(datafile, header=1, usecols=[0,1,2,3])
     shapedDF = df.pivot(index='alpha2', columns='alpha1', values='correlation')
     print(shapedDF)
-    heatmap=seaborn.heatmap(shapedDF)
-    heatmap.invert_yaxis()
+    fig, ax = plt.subplots(figsize=(11, 9))
+    # plot heatmap
+    seaborn.heatmap(shapedDF, cmap="Blues", linewidth=0.3,
+                    cbar_kws={"shrink": .8}).invert_yaxis()
+    title = 'PCA Rank Correlations\n'.upper()
+    plt.title(title, loc='left')
     plt.savefig(outfile)
     return
 
@@ -26,17 +31,24 @@ def zachKNNHeatmap(datafile, outfile):
     df = pd.read_csv(datafile, header=1, usecols=[0,1,2])
     shapedDF = df.pivot(index='alpha2', columns='alpha1', values='accuracy')
     print(shapedDF)
-    heatmap=seaborn.heatmap(shapedDF)
-    heatmap.invert_yaxis()
+    fig, ax = plt.subplots(figsize=(11, 9))
+    # plot heatmap
+    seaborn.heatmap(shapedDF, cmap="Blues", linewidth=0.3,
+                    cbar_kws={"shrink": .8}).invert_yaxis()
+    title = 'Zach KNN Average Accuracy\n'.upper()
+    plt.title(title, loc='left')
     plt.savefig(outfile)
-    return
 
 def KNNHeatmap(datafile, outfile):
     df = pd.read_csv(datafile, header=1, usecols=[0,1,2])
     shapedDF = df.pivot(index='alpha2', columns='alpha1', values='accuracy')
     print(shapedDF)
-    heatmap=seaborn.heatmap(shapedDF)
-    heatmap.invert_yaxis()
+    fig, ax = plt.subplots(figsize=(11, 9))
+    # plot heatmap
+    seaborn.heatmap(shapedDF, cmap="Blues", linewidth=0.3,
+                    cbar_kws={"shrink": .8}).invert_yaxis()
+    title = 'KNN Average Mean Squared Error\nAcross K Folds\n'.upper()
+    plt.title(title, loc='left')
     plt.savefig(outfile)
     return
 
@@ -44,8 +56,12 @@ def randomForestHeatmap(datafile, outfile):
     df = pd.read_csv(datafile, header=1, usecols=[0,1,2,3])
     shapedDF = df.pivot(index='alpha2', columns='alpha1', values='mean')
     print(shapedDF)
-    heatmap=seaborn.heatmap(shapedDF)
-    heatmap.invert_yaxis()
+    fig, ax = plt.subplots(figsize=(11, 9))
+    # plot heatmap
+    seaborn.heatmap(shapedDF, cmap="Blues", linewidth=0.3,
+                    cbar_kws={"shrink": .8}).invert_yaxis()
+    title = 'Random Forests Average Mean Squared Error\nAcross K Folds\n'.upper()
+    plt.title(title, loc='left')
     plt.savefig(outfile)
     return
 
@@ -53,9 +69,12 @@ def SVRHeatmap(datafile, outfile):
     df = pd.read_csv(datafile, header=1, usecols=[0,1,2,3])
     shapedDF = df.pivot(index='alpha2', columns='alpha1', values='mean')
     print(shapedDF)
-    heatmap=seaborn.heatmap(shapedDF)
-    heatmap.invert_yaxis()
+    fig, ax = plt.subplots(figsize=(11, 9))
+    # plot heatmap
+    seaborn.heatmap(shapedDF, cmap="Blues", linewidth=0.3,
+                    cbar_kws={"shrink": .8}).invert_yaxis()
+    title = 'SVR Average Mean Squared Error Across K Folds\n'.upper()
+    plt.title(title, loc='left')
     plt.savefig(outfile)
     return
-
 main()
